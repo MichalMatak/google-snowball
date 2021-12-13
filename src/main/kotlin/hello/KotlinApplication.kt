0d6cee520f9620fb6ff1ef2e6de89ba8815c0f56
Michał Matak <michal.matak@allegro.pl>
2021-12-13 10:39:10 +0000

    init

---
+++ src/main/kotlin/hello/KotlinApplication.kt
@@ -0,0 +1,50 @@
+package hello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
+
+@SpringBootApplication
+class KotlinApplication {
+
+    @Bean
+    fun routes() = router {
+        GET {
+            ServerResponse.ok().body(Mono.just("Let the battle begin!"))
+        }
+
+        POST("/**", accept(APPLICATION_JSON)) { request ->
+            request.bodyToMono(ArenaUpdate::class.java).flatMap { arenaUpdate ->
+                println(arenaUpdate)
+                ServerResponse.ok().body(Mono.fromCallable{
+                    strategy(arenaUpdate)
+                    })
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<KotlinApplication>(*args)
+}
+
+fun strategy(arenaUpdate: ArenaUpdate):String {
+    return listOf("R", "T").random()
+}
+
+fun findMe(arenaUpdate: ArenaUpdate):PlayerState {
+    arenaUpdate.arena.state.get("https://kotlin-springboot-jgjya6uruq-uc.a.run.app")
+}
+
+data class ArenaUpdate(val _links: Links, val arena: Arena)
+data class PlayerState(val x: Int, val y: Int, val direction: String, val score: Int, val wasHit: Boolean)
+data class Links(val self: Self)
+data class Self(val href: String)
+data class Arena(val dims: List<Int>, val state: Map<String, PlayerState>)
+
+// https://kotlin-springboot-jgjya6uruq-uc.a.run.app
